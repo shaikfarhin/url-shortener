2c72a66ed8538badd683390fa8623c27b34b5d84
Shaik Farhin <shaikfarhin1235@gmail.com>
2026-07-14 12:40:39 +0530

    Automatically create urls table

--- database/db.go
+++ database/db.go
@@ -14,7 +14,6 @@ var DB *sql.DB
 
 func ConnectDB() {
 
-	// Load .env only for local development
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using environment variables")
 	}
@@ -25,29 +24,40 @@ func ConnectDB() {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	// Render PostgreSQL requires SSL
 	psqlInfo := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-		host,
-		port,
-		user,
-		password,
-		dbname,
+		host, port, user, password, dbname,
 	)
 
 	var err error
 
-	// Initialize the global DB variable
 	DB, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal("Failed to open database:", err)
+		log.Fatal(err)
 	}
 
-	// Test the connection
 	err = DB.Ping()
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Fatal(err)
 	}
 
 	log.Println("✅ Connected to PostgreSQL")
+
+	// Create table if it doesn't exist
+	query := `
+	CREATE TABLE IF NOT EXISTS urls (
+		id SERIAL PRIMARY KEY,
+		original_url TEXT NOT NULL,
+		short_code VARCHAR(10) UNIQUE NOT NULL,
+		clicks INT DEFAULT 0,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+	`
+
+	_, err = DB.Exec(query)
+	if err != nil {
+		log.Fatal("Failed to create table:", err)
+	}
+
+	log.Println("✅ urls table is ready")
 }
